7d865400d2dbd8ef1f9163d1c52df78ea225aa26
Arthur <baginian.artur@yandex.ru>
2024-12-10 23:18:40 +0300

    Example for JWT auth & user router

--- auth_service/routers/v1/auth.py
+++ auth_service/routers/v1/auth.py
@@ -1,14 +1,32 @@
-from fastapi import APIRouter
+from typing import Annotated
 
-from auth_service.core.dependencies.fastapi import EncryptorDependency
+from fastapi import APIRouter, Depends, HTTPException, status
+
+from auth_service.core.dependencies.fastapi import DatabaseDependency, EncryptorDependency
+from auth_service.lib.db import user as users_db
 from auth_service.lib.schemas.token import TokenInfo
-from auth_service.lib.schemas.user import UserLoginSchema
+from auth_service.lib.schemas.user import UserLoginSchema, UserSchema
 
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
 
+async def validate_user(
+    db: DatabaseDependency,
+    encryptor: EncryptorDependency,
+    user_login: UserLoginSchema,
+) -> UserSchema:
+    if not (user := await users_db.get_user_by_username(db, username=user_login.username)):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    if encryptor.hash_password(user_login.password) != user.hashed_password:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    return user
+
+
 @router.post("/login", response_model=TokenInfo)
-async def login_user(user: UserLoginSchema, encryptor: EncryptorDependency) -> TokenInfo:
+async def login_user(
+    user: Annotated[UserLoginSchema, Depends(validate_user)],
+    encryptor: EncryptorDependency,
+) -> TokenInfo:
     payload = encryptor.encode_jwt(user.username)
     return TokenInfo(access_token=payload)
