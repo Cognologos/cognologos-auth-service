8eea888db41d05f21ee9b4858e6372cabf65c03b
Arthur <baginian.artur@yandex.ru>
2024-12-12 14:15:37 +0300

    Create auth_session table

---
+++ auth_service/lib/models/auth_session.py
@@ -0,0 +1,35 @@
+from datetime import datetime, timezone
+from typing import TYPE_CHECKING
+from uuid import UUID as PyUUID, uuid4
+
+from sqlalchemy import DateTime, ForeignKey, String, Uuid as SqlUUID
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from .abc import AbstractModel
+
+
+if TYPE_CHECKING:
+    from .user import UserModel
+
+
+class AuthSessionModel(AbstractModel):
+    __tablename__ = "auth_sessions"
+    id: Mapped[PyUUID] = mapped_column("id", SqlUUID(native_uuid=True, as_uuid=True), primary_key=True, default=uuid4)
+    user_id: Mapped[int] = mapped_column("user_id", ForeignKey("users.id"), nullable=False)
+    user_ip: Mapped[str] = mapped_column("user_ip", String(128), nullable=False)
+    user_agent: Mapped[str | None] = mapped_column("user_agent", String(256), nullable=True)
+    fingerprint: Mapped[str] = mapped_column("fingerprint", String(128), nullable=False)
+    access_token: Mapped[PyUUID | None] = mapped_column(
+        "access_token", SqlUUID(native_uuid=True, as_uuid=True), nullable=True, default=uuid4
+    )
+    refresh_token: Mapped[PyUUID | None] = mapped_column(
+        "refresh_token", SqlUUID(native_uuid=True, as_uuid=True), nullable=True, default=uuid4
+    )
+    last_online: Mapped[datetime] = mapped_column(
+        "last_online", DateTime(timezone=True), nullable=False, default=lambda: datetime.now(timezone.utc)
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        "created_at", DateTime(timezone=True), nullable=False, default=lambda: datetime.now(timezone.utc)
+    )
+    # relationships
+    user: Mapped["UserModel"] = relationship("UserModel")
