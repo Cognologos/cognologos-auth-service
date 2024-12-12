4d3e56f507d80b1e174213cd9ab458b77ef195c3
Arthur <baginian.artur@yandex.ru>
2024-12-12 16:18:46 +0300

    Creat auth login, logout and refresh_tokens

--- auth_service/routers/v1/auth.py
+++ auth_service/routers/v1/auth.py
@@ -1,36 +1,61 @@
-from typing import Annotated
+from fastapi import APIRouter, Response
 
-from fastapi import APIRouter, Depends
+from auth_service.core.dependencies.fastapi import (
+    ClientHostDependency,
+    DatabaseDependency,
+    EncryptorDependency,
+    RedisDependency,
+    RefreshTokenDependency,
+    TokenDataDependency,
+    UserAgentDependency,
+)
+from auth_service.lib.db import auth as auth_db, auth_session as auth_session_db
+from auth_service.lib.schemas.auth import TokenCreateSchema, TokenSchema
 
-from auth_service.core.dependencies.fastapi import DatabaseDependency, EncryptorDependency
-from auth_service.core.exceptions.user import UserUnauthorizedException
-from auth_service.lib.db import user as users_db
-from auth_service.lib.schemas.token import TokenInfo
-from auth_service.lib.schemas.user import UserLoginSchema, UserSchema
+
+router = APIRouter(tags=["auth"], prefix="/auth")
 
 
-router = APIRouter(prefix="/auth", tags=["auth"])
-
-
-async def validate_user(
+@router.post("/login", response_model=TokenSchema)
+async def login(
+    response: Response,
     db: DatabaseDependency,
+    redis: RedisDependency,
     encryptor: EncryptorDependency,
-    user_login: UserLoginSchema,
-) -> UserSchema:
-    user = await users_db.get_user_by_username(db, username=user_login.username)
-    if encryptor.hash_password(user_login.password) != user.hashed_password:
-        raise UserUnauthorizedException
-    return user
-
-
-@router.post("/login", response_model=TokenInfo)
-async def login_user(
-    user: Annotated[UserLoginSchema, Depends(validate_user)],
-    encryptor: EncryptorDependency,
-) -> TokenInfo:
-    access_token = encryptor.encode_jwt(user.username)
-    refresh_token = encryptor.encode_refresh_jwt(user.username)
-    return TokenInfo(
-        access_token=access_token,
-        refresh_token=refresh_token,
+    user_agent: UserAgentDependency,
+    client_host: ClientHostDependency,
+    schema: TokenCreateSchema,
+) -> TokenSchema:
+    result = await auth_db.create_token(db, redis, encryptor, client_host, user_agent, schema)
+    response.set_cookie(
+        "refresh_token", result.refresh_token, httponly=True, max_age=result.refresh_token_expires_in * 60 * 60 * 24
     )
+    return result
+
+
+@router.post("/refresh_tokens", response_model=TokenSchema)
+async def refresh_tokens(
+    response: Response,
+    db: DatabaseDependency,
+    redis: RedisDependency,
+    encryptor: EncryptorDependency,
+    user_agent: UserAgentDependency,
+    client_host: ClientHostDependency,
+    refresh_token: RefreshTokenDependency,
+) -> TokenSchema:
+    result = await auth_db.refresh_token(db, redis, encryptor, client_host, user_agent, refresh_token)
+    response.set_cookie(
+        "refresh_token", result.refresh_token, httponly=True, max_age=result.refresh_token_expires_in * 60 * 60 * 24
+    )
+    return result
+
+
+@router.delete("/logout", status_code=204)
+async def logout(
+    response: Response,
+    db: DatabaseDependency,
+    redis: RedisDependency,
+    token_data: TokenDataDependency,
+) -> None:
+    response.delete_cookie("refresh_token")
+    return await auth_session_db.delete_auth_session(db, redis, token_data.session_id, token_data.user_id)
